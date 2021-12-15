d759062cae38cbf49adc363e3d7711917dd3ce11
ayyashmuharram <95995787+ayyashmuharram@users.noreply.github.com>
2021-12-15 17:24:47 +0700

    Update ayoil.py

--- ayoil.py
+++ ayoil.py
@@ -1,6 +1,7 @@
 """
-Aplikasi Streamlit untuk menggambarkan statistik produksi minyak
-
+Aplikasi Streamlit untuk menggambarkan statistik penumpang TransJakarta
+Sumber data berasal dari Jakarta Open Data
+Referensi API Streamlit: https://docs.streamlit.io/library/api-reference
 """
 
 import numpy as np
@@ -9,63 +10,22 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import streamlit as st
 from PIL import Image
-import json
+
 
 ############### title ###############
 st.set_page_config(layout="wide")  # this needs to be the first Streamlit command called
-st.title("Statistik Produksi Minyak")
-############### title ###############
+st.title("Statistik Jumlah Penumpang TransJakarta Tahun 2019")
+st.markdown("*Sumber data berasal dari [Jakarta Open Data](https://data.jakarta.go.id/dataset/data-jumlah-penumpang-trans-jakarta-tahun-2019-kpi)*")
+############### title ###############)
 
 ############### sidebar ###############
-image = Image.open('blackoil.png')
+image = Image.open('blp.png')
 st.sidebar.image(image)
 
-"""
-Aplikasi Streamlit untuk menggambarkan statistik produksi minyak
-"""
+st.sidebar.title("Pengaturan")
+left_col, mid_col, right_col = st.columns(3)
 
-print('Blackoil in your area')
-df1 = pd.read_csv("produksi_minyak_mentah.csv")
-#print(df1)
-
-file_json = open("kode_negara_lengkap.json")
-data = json.loads(file_json.read())
-
-#konversi kode ke nama
-
-# cetak isi data JSON
-#print(data[0]['alpha-3'],data[0]['name'])
-#print(df1.loc[0,"kode_negara"])
-#print(type(data))
-#print(type(df1))
-#print(len(data))
-
-
-#apakah json bisa iterasi
-#urutan=0
-#for i in data:
-#    print(data[urutan]['alpha-3'],data[urutan]['name'])
-#   urutan=urutan+1
-
-urutan=0
-for i in df1.loc():
-    urutan2=0
-    for j in data:
-        kode1=(df1.loc[urutan,"kode_negara"])
-        kode2=data[urutan2]['alpha-2']
-        kode3=data[urutan2]['alpha-3']
-        #print("kode 1 adalah",kode1)
-        #print("kode 2 adalah",kode2)
-        #print("kode 3 adalah",kode3)
-        if kode1==kode2 or kode1==kode3:
-            df1.loc[urutan,"kode_negara"]=data[urutan2]['name']
-            #print('negara berubah menjadi',df1.loc[urutan,"kode_negara"])
-        #print("ini adalah urutan kode negara",urutan,"dan urutan json",urutan2)
-        urutan2=urutan2+1
-    urutan=urutan+1
-    if urutan==5839:break
-print(df1)
-
+## User inputs on the control panel
 st.sidebar.subheader("Pengaturan konfigurasi tampilan")
 list_bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
 bulan = st.sidebar.selectbox("Pilih bulan", list_bulan)
@@ -89,3 +49,102 @@ n_tampil = st.sidebar.number_input("Jumlah baris dalam tabel yang ditampilkan", 
 ############### upper left column ###############
 left_col.subheader("Tabel representasi data")
 
+filepath = list_url[bulan]
+df = pd.read_csv(filepath)
+
+left_col.dataframe(df.head(n_tampil))
+############### upper left column ###############
+
+############### upper middle column ###############
+mid_col.subheader("Jenis transportasi")
+
+jenis_unik = list(df['jenis'].unique())
+tulis_jenis = []
+for i, jenis in enumerate(jenis_unik):
+    tulis_jenis.append(f"{str(i+1)}. {jenis}\n")
+tulis_jenis = ' '.join(map(str, tulis_jenis))
+
+mid_col.markdown(tulis_jenis)
+############### upper middle column ###############
+
+############### upper right column ###############
+right_col.subheader("Total penumpang perbulan")
+
+total_perbulan = []
+for i in range(len(list_bulan)):
+    df_bulan = pd.read_csv(list_url[list_bulan[i]])
+    df_bulan['jumlah_penumpang'] = df_bulan['jumlah_penumpang'].str.replace(",", "", regex=True)
+    df_bulan['jumlah_penumpang'] = df_bulan['jumlah_penumpang'].str.replace(".", "", regex=True)
+    df_bulan['jumlah_penumpang'] = pd.to_numeric(df_bulan['jumlah_penumpang'], errors='coerce')
+    #jumlah_perbulan = df_bulan['jumlah_penumpang'].astype(int).sum()
+    jumlah_perbulan = df_bulan['jumlah_penumpang'].sum()
+    #print(f"Bulan {bulan}, total penumpang: {jumlah_perbulan}")
+    total_perbulan.append(int(jumlah_perbulan))
+
+cmap_name = 'tab20'
+cmap = cm.get_cmap(cmap_name)
+colors = cmap.colors[:len(list_bulan)]
+fig, ax = plt.subplots()
+ax.bar(list_bulan, total_perbulan, color=colors)
+ax.set_xticklabels(list_bulan, rotation=45)
+ax.set_xlabel("Bulan", fontsize=12)
+ax.set_ylabel("Total jumlah penumpang", fontsize=12)
+plt.tight_layout()
+
+right_col.pyplot(fig)
+############### upper right column ###############
+
+############### lower left column ###############
+left_col.subheader("Total penumpang")
+
+total_penumpang = []
+for jenis in jenis_unik:
+    df['jumlah_penumpang'] = df['jumlah_penumpang'].str.replace(",", "", regex=True)
+    df['jumlah_penumpang'] = df['jumlah_penumpang'].str.replace(".", "", regex=True)
+    #df['jumlah_penumpang'] = pd.to_numeric(df['jumlah_penumpang'], errors='coerce')
+    jumlah_penumpang = df[df['jenis']==jenis]['jumlah_penumpang'].astype(int)
+    total_penumpang.append(jumlah_penumpang.sum())
+
+fig, ax = plt.subplots()
+ax.barh(jenis_unik, total_penumpang, color=colors)
+ax.set_yticklabels(jenis_unik, rotation=0)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel("Total jumlah penumpang", fontsize=12)
+
+left_col.pyplot(fig)
+############### lower left column ###############
+
+############### lower middle column ###############
+mid_col.subheader("Jumlah trayek")
+
+total_trayek = []
+for jenis in jenis_unik:
+    jumlah_trayek = df[df['jenis']==jenis].shape[0]
+    total_trayek.append(jumlah_trayek)
+
+def label_function(val):
+    return f'{val / 100 * sum(total_trayek):.0f}'
+
+colors = cmap.colors[:len(jenis_unik)]
+fig, ax = plt.subplots()
+patches, labels, texts = ax.pie(total_trayek, autopct=label_function, startangle=90, colors=colors, textprops={'fontsize': 11})
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ax.legend(patches, jenis_unik, loc="center left", bbox_to_anchor=(0.85, 0.5))
+
+mid_col.pyplot(fig)
+############### lower middle column ###############
+
+############### lower right column ###############
+right_col.subheader("Summary")
+max_penumpang = np.asarray(total_penumpang).max()
+max_penumpang_idx = np.asarray(total_penumpang).argmax()
+right_col.markdown(f"**Jenis angkutan terpadat pada {bulan}: ** \n {jenis_unik[max_penumpang_idx]} ({max_penumpang} orang)")
+
+max_trayek = np.asarray(total_trayek).max()
+max_trayek_idx = np.asarray(total_trayek).argmax()
+right_col.markdown(f"**Jenis trayek terbanyak pada {bulan}: ** \n {jenis_unik[max_trayek_idx]} ({max_trayek} trayek)")
+
+max_perbulan = np.asarray(total_perbulan).max()
+max_perbulan_idx = np.asarray(total_perbulan).argmax()
+right_col.markdown(f"**Bulan tersibuk adalah: ** \n {list_bulan[max_perbulan_idx]} ({max_perbulan} orang)")
+############### lower right column ###############
