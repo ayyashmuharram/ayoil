dad8f7dcbc1e47518ab4e953512ce7d150af3e1f
ayyashmuharram <95995787+ayyashmuharram@users.noreply.github.com>
2021-12-15 18:21:50 +0700

    Update ayoil.py

--- ayoil.py
+++ ayoil.py
@@ -24,6 +24,6 @@ st.sidebar.image(image)
 
 # read dataset
 #filepath = "data-penumpang-bus-transjakarta-desember-2019.csv"
-filepath = 'https://data.jakarta.go.id/dataset/50b36c4b-0aed-42a5-82e4-c3510475716a/resource/4c3be51b-6ae9-44cc-9b42-616b4c982614/download/Data-Penumpang-Bus-Transjakarta-Desember-2019.csv'
-df = pd.read_csv(filepath)
+#filepath = 'https://data.jakarta.go.id/dataset/50b36c4b-0aed-42a5-82e4-c3510475716a/resource/4c3be51b-6ae9-44cc-9b42-616b4c982614/download/Data-Penumpang-Bus-Transjakarta-Desember-2019.csv'
+df = pd.read_csv(produksi_minyak_mentah.csv)
 df
